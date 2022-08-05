1ae86afed97b3518400371ebc4da56f3185e8a8e
surefire01 <harshalawaghadsa7@gmail.com>
2022-08-05 22:25:50 +0530

    made Wrapper a stateless widget form statefull widget

--- lib/screens/Wrapper/wrapper.dart
+++ lib/screens/Wrapper/wrapper.dart
@@ -8,18 +8,30 @@ import 'package:provider/provider.dart';
 // this is not a page but this widgets checks if the useris logged in or not and based on that shows home scree or authentiate
 
 
-class Wrapper extends StatefulWidget {
+// class Wrapper extends StatefulWidget {
+//   const Wrapper({Key? key}) : super(key: key);
+//
+//   @override
+//   State<Wrapper> createState() => _WrapperState();
+// }
+//
+// class _WrapperState extends State<Wrapper> {
+//   @override
+//   Widget build(BuildContext context) {
+//     final user = Provider.of<UserUid?>(context);
+//     print(user?.uid);
+//     return user == null ? Authenticate() : Home();
+//
+//   }
+// }
+
+class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
 
   @override
-  State<Wrapper> createState() => _WrapperState();
-}
-
-class _WrapperState extends State<Wrapper> {
-  @override
   Widget build(BuildContext context) {
     final user = Provider.of<UserUid?>(context);
-    print(user?.uid);
+    //print(user?.uid);
     return user == null ? Authenticate() : Home();
 
   }
