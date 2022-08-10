ec4f3b54a57d3fa648baffb52c9e5f7a56d080c9
surefire01 <harshalawaghadsa7@gmail.com>
2022-08-10 12:19:25 +0530

    used Page view to change Navigate feom the home screen. to do that changed file names and slightly changed structure

--- lib/screens/base_screens/events.dart
+++
@@ -1,4 +0,0 @@
-import 'package:digimanage/models/base_screen.dart';
-import 'package:flutter/material.dart';
-
-BaseScreen events = BaseScreen(body: Text("No events yet"), appBar: AppBar(title: Text("Events"),));
